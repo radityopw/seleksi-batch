9331bde55c2a7c07edfc8e6ad8341b7309ac542c
radityopw <radityo_pw@is.its.ac.id>
2021-09-13 09:33:57 +0700

    using php ext

--- result.php
+++ result.php
@@ -1,9 +1,8 @@
-#! /usr/bin/php
 <?php
 error_reporting(E_ALL);
 
 chdir(dirname(__FILE__));
 
-require_once 'config';
+require_once 'config.php';
 
 passthru($sqlite_exec." ".$db_location." < export_hasil.sql");
